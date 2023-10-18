cdbfc4dbbe124fbdd18439c15b9d95f3db172ce7
Daniel Njama Wangari <daniel.njama@prodapt.com>
2023-10-18 13:09:45 +0300

    added pagination

--- drfapp/views.py
+++ drfapp/views.py
@@ -2,16 +2,18 @@ from django.shortcuts import render
 from . models import students
 from . serializers import StudentSerializer
 from rest_framework import generics
-# from rest_framework.response import Response
-# from rest_framework import status
-# from rest_framework.views import APIView
-# from django.http import Http404
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class StudentList(generics.ListCreateAPIView):
     queryset = students.objects.all()
     serializer_class= StudentSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class StudentDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = students.objects.all()
     serializer_class= StudentSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
